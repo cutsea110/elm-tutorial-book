54dfc8e28f1549486f519cf27d9da0ff5cad7aa8
cutsea110 <cutsea110@gmail.com>
2021-03-13 11:10:38 +0900

    refactor

--- src/Main.elm
+++ src/Main.elm
@@ -7,10 +7,20 @@ import Html.Attributes exposing (href)
 main : Html msg
 main =
     div []
-        [ h1 [] [ text "Useful Links" ]
-        , ul []
-            [ li [] [ a [ href "https://elm-lang.org" ] [ text "Homepage" ] ]
-            , li [] [ a [ href "https://package.elm-lang.org" ] [ text "Packages" ] ]
-            , li [] [ a [ href "https://ellie-app.com" ] [ text "Playground" ] ]
-            ]
+        [ header
+        , content
+        ]
+
+
+header : Html msg
+header =
+    h1 [] [ text "Useful Links" ]
+
+
+content : Html msg
+content =
+    ul []
+        [ li [] [ a [ href "https://elm-lang.org" ] [ text "Homepage" ] ]
+        , li [] [ a [ href "https://package.elm-lang.org" ] [ text "Packages" ] ]
+        , li [] [ a [ href "https://ellie-app.com" ] [ text "Playground" ] ]
         ]
